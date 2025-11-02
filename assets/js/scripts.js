11247a89ae75bba2e9140c050cd5e0b8fc2e9574
Ruslan <info@mirasov.dev>
2025-11-02 18:10:01 +0100

    costumes page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,12 @@
 import { initNavigationMenu, hidePreloader, initLinksMiddleware, initScrollArea } from './helpers.js';
 import { initSliders, onStageSlideChange } from './sliders.js';
+import { initTabs } from './tabs.js';
 
 initScrollArea();
 initNavigationMenu();
 initLinksMiddleware();
 initSliders();
+initTabs();
 hidePreloader();
 
 let lastRealIndex = null;
---
+++ assets/js/tabs.js
@@ -0,0 +1,42 @@
+export const initTabs = () => {
+  const tabs = document.querySelector('[data-tabs]');
+
+  if (!tabs) return;
+
+  const handleTabsClick = e => {
+    const target = e.target;
+    if (!target.hasAttribute('data-tab-link')) return;
+
+    const activeButton = tabs.querySelector('.tab-button.active');
+
+    if (target === activeButton) return;
+
+    const number = target.dataset.tabLink;
+    const activeTab = tabs.querySelector('[data-tab].active');
+    const targetButton = tabs.querySelector(`[data-tab-link="${number}"]`);
+    const targetTab = tabs.querySelector(`[data-tab="${number}"]`);
+
+    activeButton.classList.remove('active');
+    targetButton.classList.add('active');
+
+    activeTab.style.zIndex = '19';
+    targetTab.style.zIndex = '19';
+    activeTab.style.opacity = '0';
+
+    setTimeout(() => {
+      activeTab.classList.remove('active');
+      targetTab.classList.add('active');
+    }, 500);
+
+    setTimeout(() => {
+      targetTab.style.opacity = '1';
+    }, 550);
+
+    setTimeout(() => {
+      activeTab.style.zIndex = '';
+      targetTab.style.zIndex = '';
+    }, 1050);
+  };
+
+  tabs.addEventListener('click', handleTabsClick);
+};
